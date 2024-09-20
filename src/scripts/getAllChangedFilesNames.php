492fc92c3b087001caf90941820c19fe57aa72cd
MelleRoffelAvocado <155441984+MelleRoffelAvocado@users.noreply.github.com>
2024-09-20 14:58:26 +0200

    removed comments

--- src/scripts/getAllChangedFilesNames.php
+++ src/scripts/getAllChangedFilesNames.php
@@ -21,24 +21,3 @@ function removeEmptyArrayItems(array &$gitCommandOutputArray)
         }
     }
 }
-//
-//$gitStatusResult = shell_exec("git status --porcelain | grep '^M[ ]*'");
-//
-//if ($config['modified_files_only'] === false) {
-//    $gitStatusResult = shell_exec("git status --porcelain | grep '^[AM] '");
-//}
-//
-//if ($gitStatusResult == null) {
-//    return [];
-//}
-//$gitStatusResultArray = explode(PHP_EOL,  trim($gitStatusResult));
-//
-//for ($i = 0; $i < count($gitStatusResultArray); $i++) {
-//    $gitStatusResultArray[$i] = preg_replace('/^\w{2}\s/', '', $gitStatusResultArray[$i]);
-//    $gitStatusResultArray[$i] = preg_replace('/^M\s*/', '', $gitStatusResultArray[$i]);
-//    if ($config['modified_files_only']) {
-//        $gitStatusResultArray[$i] = preg_replace('/^A{1,2}\s*/', '', $gitStatusResultArray[$i]);
-//    }
-//}
-//
-//return $gitStatusResultArray;
